557b1687bc8693c122de346257db6f941cf88ea2
Jigyasu-vatsa <62921979+Jigyasu-vatsa@users.noreply.github.com>
2025-02-09 05:11:32 +0530

    Update resume.js

--- js/resume.js
+++ js/resume.js
@@ -4,9 +4,33 @@ document.getElementById("exprdmore").addEventListener("click",myreadmorefunc);
 
 function myreadmorefunc()
 {
-    document.getElementById ("expreadmorecntnt").classList.toggle('showMe')
+    document.getElementById ("expreadmorecntnt").classList.toggle('showMe');
     
     document.getElementById("exprdmore").style.display="none";
+
+}
+
+// Experience section KnowMore Script
+
+document.getElementById("exprdmore1").addEventListener("click",myreadmorefunc1);
+
+function myreadmorefunc1()
+{
+    document.getElementById ("expreadmorecntnt1").classList.toggle('showMe');
+    
+    document.getElementById("exprdmore1").style.display="none";
+
+}
+
+// Experience section KnowMore Script
+
+document.getElementById("exprdmore2").addEventListener("click",myreadmorefunc2);
+
+function myreadmorefunc2()
+{
+    document.getElementById ("expreadmorecntnt2").classList.toggle('showMe');
+    
+    document.getElementById("exprdmore2").style.display="none";
 
 }
 
